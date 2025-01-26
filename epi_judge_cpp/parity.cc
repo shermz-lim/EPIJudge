9a72321e086183de3f8d1080a9d2af67518f446f
Sherman Lim <shermz.lim.jh@gmail.com>
2025-01-26 21:29:55 +0800

    update parity to use constexpr

--- epi_judge_cpp/parity.cc
+++ epi_judge_cpp/parity.cc
@@ -1,13 +1,14 @@
 #include "test_framework/generic_test.h"
-short Parity(unsigned long long x) {
-  static const std::vector<bool> lookup_table = []() {
-    // initialize parity lookup table for 16 bit integers
-    size_t size = 1 << 16;
-    std::vector<bool> table(size);
 
-    for (size_t x = 0; x < size; x++) {
-      size_t tmp = x;
-      for (size_t width = 8; width > 0; width >>= 1) {
+short Parity(unsigned long long x) {
+  // initialize parity lookup table for 16 bit integers
+  constexpr int table_sz = 1 << 16;
+  static constexpr std::array<bool, table_sz> lookup_table = []() constexpr {
+    std::array<bool, table_sz> table{};
+
+    for (int x = 0; x < table_sz; ++x) {
+      int tmp = x;
+      for (int width = 8; width > 0; width >>= 1) {
         tmp ^= (tmp >> width);
       }
       tmp &= 0x1;
