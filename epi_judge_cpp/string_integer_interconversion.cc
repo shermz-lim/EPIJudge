dacf7a3b4d7b2a94d1443cbc21ff4218da7af6b2
Sherman Lim <shermz.lim.jh@gmail.com>
2025-01-27 13:19:38 +0800

    add string_integer_interconversion

--- epi_judge_cpp/string_integer_interconversion.cc
+++ epi_judge_cpp/string_integer_interconversion.cc
@@ -5,12 +5,38 @@
 using std::string;
 
 string IntToString(int x) {
-  // TODO - you fill in here.
-  return "0";
+  bool neg = x < 0;
+
+  std::string res{};
+  do {
+    res.push_back(abs(x % 10) + '0');
+    x /= 10;
+  } while (x);
+
+  if (neg) {
+    res.push_back('-');
+  }
+  std::reverse(res.begin(), res.end());
+
+  return res;
 }
 int StringToInt(const string& s) {
-  // TODO - you fill in here.
-  return 0;
+  int i = 0;
+  bool neg = false;
+  if (s[i] == '-') {
+    i++;
+    neg = true;
+  } else if (s[i] == '+') {
+    i++;
+  }
+
+  int x = 0;
+  for (; i < s.size(); i++) {
+    x *= 10;
+    x += (s[i] - '0');
+  }
+
+  return neg ? -x : x;
 }
 void Wrapper(int x, const string& s) {
   if (stoi(IntToString(x)) != x) {
