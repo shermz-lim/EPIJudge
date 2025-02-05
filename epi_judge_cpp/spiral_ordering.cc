d6a4ee53b8ea00b68760065692866d5d088399d6
Sherman Lim <shermz.lim.jh@gmail.com>
2025-02-06 00:50:45 +0800

    add spiral_ordering

--- epi_judge_cpp/spiral_ordering.cc
+++ epi_judge_cpp/spiral_ordering.cc
@@ -3,8 +3,31 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 vector<int> MatrixInSpiralOrder(const vector<vector<int>>& square_matrix) {
-  // TODO - you fill in here.
-  return {};
+  vector<int> res{};
+  for (
+    int low = 0, high = square_matrix.size() - 1;
+    low <= high;
+    low++, high--
+  ) {
+    if (low == high) {
+      res.push_back(square_matrix[low][low]);
+      break;
+    }
+
+    for (int c = low; c < high; ++c) {
+      res.push_back(square_matrix[low][c]);
+    }
+    for (int r = low; r < high; ++r) {
+      res.push_back(square_matrix[r][high]);
+    }
+    for (int c = high; c > low; --c) {
+      res.push_back(square_matrix[high][c]);
+    }
+    for (int r = high; r > low; --r) {
+      res.push_back(square_matrix[r][low]);
+    }
+  }
+  return res;
 }
 
 int main(int argc, char* argv[]) {
